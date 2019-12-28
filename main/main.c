a03feefb9ec7a1e1ff226148df3335db2cce1176
Andrew Ostroumov <andrew.ostroumov@gmail.com>
2019-12-28 22:38:51 +0200

    Update dev driver

--- main/main.c
+++ main/main.c
@@ -11,9 +11,6 @@
 #define I2C_MASTER_SDA_IO           18
 #define I2C_MASTER_FREQ_HZ          100000
 
-#undef BME280_CHIP_ID
-#define BME280_CHIP_ID              UINT8_C(0x58)
-
 typedef struct {
     double temperature;
     double pressure;
@@ -231,17 +228,13 @@ void task_dev_read(void *param)
 }
 void app_main(void)
 {
+    // Just because I want to power dev from gpio so remove it
     gpio_config_t io_conf;
-    //disable interrupt
     io_conf.intr_type = GPIO_PIN_INTR_DISABLE;
-    //set as output mode
     io_conf.mode = GPIO_MODE_OUTPUT;
     io_conf.pin_bit_mask = GPIO_OUTPUT_SEL;
-    //disable pull-down mode
     io_conf.pull_down_en = 0;
-    //disable pull-up mode
     io_conf.pull_up_en = 0;
-    //configure GPIO with the given settings
     gpio_config(&io_conf);
     gpio_set_level(GPIO_OUTPUT_VOL, true);
 
